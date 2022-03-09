41cfb16f413e08b9dda51e86e047c6524b8f7ffc
Hoàng Trương Minh <tmhhh@tmhhh.local>
2022-03-09 23:25:32 +0700

    custom bottom tab

--- src/navigators/bottomTab.js
+++ src/navigators/bottomTab.js
@@ -4,12 +4,33 @@ import Home from '../screens/Home';
 import UserProfile from '../screens/UserProfile';
 import MatchStack from './matchStack';
 import StatisticStack from './statisticStack';
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import colors from '../../assets/color';
 const Tab = createBottomTabNavigator();
 
 export default function BottomTab() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Match') {
+            iconName = focused ? 'football' : 'football-outline';
+          } else if (route.name === 'Statistic') {
+            iconName = focused ? 'stats-chart' : 'stats-chart-outline';
+          } else {
+            iconName = focused ? 'person' : 'person-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={colors.black} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}>
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Match" component={MatchStack} />
       <Tab.Screen name="Statistic" component={StatisticStack} />
