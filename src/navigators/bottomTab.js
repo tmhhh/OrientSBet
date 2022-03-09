2b0e0b4943d0937de86c7cf156c2fb39b7bc6f95
thelastwizardOC <tdtri.115@gmail.com>
2022-03-09 13:14:01 +0700

    organize project

---
+++ src/navigators/bottomTab.js
@@ -0,0 +1,22 @@
+import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+import LoginStack from './loginStack';
+import MatchStack from './matchStack';
+import StatisticStack from './statisticStack';
+import UserProfile from '../screens/UserProfile';
+import Home from '../screens/Home';
+
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+export default function BottomTab() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={LoginStack} />
+      <Tab.Screen name="Match" component={MatchStack} />
+      <Tab.Screen name="Statistic" component={StatisticStack} />
+      <Tab.Screen name="UserProfile" component={UserProfile} />
+    </Tab.Navigator>
+  );
+}
