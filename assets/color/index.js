d86418a198d765b5c9b93aa61a3728bffb84c1fb
thelastwizardOC <tdtri.115@gmail.com>
2022-03-09 13:29:53 +0700

    add color

---
+++ assets/color/index.js
@@ -0,0 +1,11 @@
+const colors = {
+  orange: '#F94B02',
+  grey: '#999',
+  lightGrey: '#C4C4C4',
+  mediumGrey: '#ADB5BD',
+  darkGrey: '#495057',
+  black: '#000',
+  white: '#fff',
+};
+
+export default colors;
